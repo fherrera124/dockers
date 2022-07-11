c5a7db3ec9807ace0be87a03be0a1a6933d5b678
Francisco Herrera <fran.herrera124@gmail.com>
2022-07-10 23:20:30 -0300

    workaround to get OPDS Catalog on koreader.  https://github.com/koreader/koreader/issues/3953

--- calibre-server/start-calibre-server.sh
+++ calibre-server/start-calibre-server.sh
@@ -4,5 +4,6 @@ XDG_RUNTIME_DIR=/tmp/runtime-root /usr/bin/calibre-server \
     --disable-use-bonjour \
     --userdb /users/users.sqlite \
     --enable-auth \
+    --auth-mode 'basic' \
     "$@" \
     "/library"
